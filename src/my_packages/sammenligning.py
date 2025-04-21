e412a512574631b28942f73f18dae7523776a2dc
Simen <simeab@stud.ntnu.no>
2025-04-21 16:46:49 +0200

    Endret navn fra lesCsv til lesogplottCsv siden det ga mer mening med dens funksjon og funksjonsnavn

--- src/my_packages/sammenligning.py
+++ src/my_packages/sammenligning.py
@@ -1,23 +1,41 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-from my_packages.lesCsv import TidsseriePlotter
-# import os as os
+from .lesogplottCsv import TidsseriePlotter
+
 
 
 
 
 def sammenlign_plot(plot1: TidsseriePlotter, plot2: TidsseriePlotter, tittel="Sammenligning"):
-    df1 = pd.DataFrame(list(plot1.data_dict.items()), columns=["Tidspunkt", "Verdi"])
-    df2 = pd.DataFrame(list(plot2.data_dict.items()), columns=["Tidspunkt", "Verdi"])
+    df1 = plot1.agg_df()
+    df2 = plot2.agg_df()
 
-    # Lager graf
-    plt.figure(figsize=(12,6))
-    plt.plot(df1["Tidspunkt"], df1["Verdi"], label=plot1.visningsnavn)
-    plt.plot(df2["Tidspunkt"], df2["Verdi"], label=plot2.visningsnavn)
+
+    plt.figure(figsize=(12, 6))
+
+    # Velg riktig plot for plot1
+    if plot1.graf_type == 'line':
+        plt.plot(df1["Tidspunkt"], df1["Verdi"], label=plot1.visningsnavn)
+    elif plot1.graf_type == 'bar':
+        plt.bar(df1["Tidspunkt"], df1["Verdi"], alpha=0.5, label=plot1.visningsnavn)
+    elif plot1.graf_type == 'scatter':
+        plt.scatter(df1["Tidspunkt"], df1["Verdi"], label=plot1.visningsnavn)
+
+    # Velg riktig plot for plot2
+    if plot2.graf_type == 'line':
+        plt.plot(df2["Tidspunkt"], df2["Verdi"], label=plot2.visningsnavn)
+    elif plot2.graf_type == 'bar':
+        plt.bar(df2["Tidspunkt"], df2["Verdi"], alpha=0.5, label=plot2.visningsnavn)
+    elif plot2.graf_type == 'scatter':
+        plt.scatter(df2["Tidspunkt"], df2["Verdi"], label=plot2.visningsnavn)
+
+    assert plot2.periode == plot1.periode, 'Plotene må ha lik periode'
+
 
     plt.title(tittel)
     plt.xlabel("Tid")
     plt.ylabel(f"{plot1.visningsnavn} / {plot2.visningsnavn}")
     plt.legend()
     plt.grid(True)
+    plt.tight_layout()
     plt.show()
